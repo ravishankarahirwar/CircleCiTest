071be7bca5cdc8ed290a8d5a9dc187742fdd04b9
ravishankar <ravishankar.ahirwar@speridian.com>
2018-06-14 16:55:18 +0530

    One Test Case Added

--- app/src/main/java/com/example/ravi/circlecitest/MainActivity.kt
+++ app/src/main/java/com/example/ravi/circlecitest/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.ravi.circlecitest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
+        showTost()
+    }
+
+    fun showTost() {
+        Toast.makeText(this@MainActivity, "This is test", Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/test/java/com/example/ravi/circlecitest/MainActivityTest.kt
@@ -0,0 +1,11 @@
+package com.example.ravi.circlecitest
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class MainActivityTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
